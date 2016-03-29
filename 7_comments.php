6d3d9c0cc510690645bf1492c3bdcac9fae4d625
Adam <miknevinas@gmail.com>
2016-03-29 13:45:29 -0600

    comments

---
+++ 7_comments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    
+    //This is a php comment
+    /*More comments*/
+        echo "Hello student";
+    
+    ?>
+</body>
+</html>
