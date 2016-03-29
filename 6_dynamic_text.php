a2ab4a2baf725cdd3b5e9524c69109383e531bac
Adam <miknevinas@gmail.com>
2016-03-29 13:39:27 -0600

    dynamic text

---
+++ 6_dynamic_text.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php 
+    $title = "Adam McNevin Official Site"
+    ?>
+    <h1><?php echo $title ?></h1>
+    
+</body>
+</html>
