907759b6bff3e1ea047cb98f4f01876817ddb604
Adam <miknevinas@gmail.com>
2016-03-29 14:41:20 -0600

    math

---
+++ 10_math.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    echo 56 + 45;
+    echo "<br />";
+    echo 36 / 6;
+    echo "<br />";
+    echo "<br />";
+    echo 45 + 3  * 21 / 3 - 5;
+    
+    echo "<br />";
+    echo "<br />";
+    $number1 = 1;
+    $number2 = 2;
+    
+    echo $number1 + $number2;
+    ?>
+</body>
+</html>
