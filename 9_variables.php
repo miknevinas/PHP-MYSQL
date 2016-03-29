8c41bdb2c97268af739fcce69b7cc56df82aab9b
Adam <miknevinas@gmail.com>
2016-03-29 14:27:50 -0600

    variables

---
+++ 9_variables.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+    //Variable names are case sensitive
+    $name = "Adam";
+    $NAME = "McNevin";
+    $number = 12;
+    
+    echo $name;
+    echo " ";
+    //Concatenation
+    echo $NAME . " " . $number;
+    
+    //Embed HTML
+    $embed = "<h1>HELO</h1>";
+    
+    echo $embed;
+    ?>
+</body>
+</html>
