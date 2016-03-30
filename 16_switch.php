21f03bc1187f3c4610d557321b1f04daeb92c25f
Adam <miknevinas@gmail.com>
2016-03-30 09:59:53 -0600

    switch

---
+++ 16_switch.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $number = 10;
+        
+        switch($number) {
+            case 34:
+            echo: "true";
+            break;
+            case 10:
+            echo: "true";
+            break;
+            case 180:
+            echo: "true";
+            break;
+                
+            default:
+            echo: "couldn't find it"
+            break;
+        }
+    ?>
+</body>
+</html>
