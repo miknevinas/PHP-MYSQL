0d2d09f9f99ecd251577aa0c33497808d9bb3966
Adam <miknevinas@gmail.com>
2016-03-30 10:33:38 -0600

    functions

---
+++ 21_functions.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    function saySomething(){
+        echo "Hello";
+    }
+    saySomething();
+    ?>
+</body>
+</html>
