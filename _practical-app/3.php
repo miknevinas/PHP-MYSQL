7a223f504ab945b9e4b98d0ae28f412e639a9362
Adam <miknevinas@gmail.com>
2016-03-30 10:26:43 -0600

    practice 3

--- _practical-app/3.php
+++ _practical-app/3.php
@@ -13,19 +13,47 @@
 <article class="main-content col-xs-8">
 
 <?php  
-
 /*  Step1: Make an if Statement with elseif and else to finally display string saying, I love PHP
 
+    Step 2: Make a forloop  that displays 10 numbers
 
+    Step 3 : Make a switch Statement that test againts one condition with 5 cases*/
 
-	Step 2: Make a forloop  that displays 10 numbers
-
-
-	Step 3 : Make a switch Statement that test againts one condition with 5 cases
-
- */
-
-	
+    //Step 1
+    $number = 12;
+    if (!$number) {
+        echo "Not real";
+    } else if ($number < 2) {
+        echo "I hate PHP";
+    } else {
+        echo "I love PHP";
+    }
+    echo "<br >"; echo "<br >";
+    
+    //Step 2
+    for($counter = 0; $counter <= 10; $counter++) {
+        echo $counter;
+    }
+    echo "<br >"; echo "<br >";
+    
+    //Step 3
+    switch ($number) {
+        case 10:
+            echo "this is the number";
+            break;
+        case 11:
+            echo "this is the number";
+            break;
+        case 13:
+            echo "this is the number";
+            break;
+        case 14:
+            echo "this is the number";
+            break;
+        case 12:
+            echo "this is the number";
+            break;
+    }
 ?>
 
 
