14416e4aa939da64bf4eaf9d37eaa12d16fd6135
Adam <miknevinas@gmail.com>
2016-03-30 12:28:12 -0600

    practice 5

--- _practical-app/5.php
+++ _practical-app/5.php
@@ -14,16 +14,26 @@
 	
 	<?php 
 
+   /* Step1: Use a pre-built math function here and echo it
 
-/*  Step1: Use a pre-built math function here and echo it
+      Step 2:  Use a pre-built string function here and echo it
 
+      Step 3:  Use a pre-built Array function here and echo it */
 
-	Step 2:  Use a pre-built string function here and echo it
-
-
-	Step 3:  Use a pre-built Array function here and echo it
-
- */
+    //Step 1
+    $number = 10;
+    echo sqrt($number);
+    echo "<br />";
+    
+    //Step 2
+    $string = "HI It's me thE wOndeR man.";
+    echo strrev($string);
+    echo "<br />";
+    
+    //Step 3
+    $arr1 = array(1,2,3);
+    $result = array_sum($arr1);
+    echo $result;
 
 	
 ?>
