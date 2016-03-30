226c315714638caf005a2d2e46993922ad08acd3
Adam <miknevinas@gmail.com>
2016-03-30 09:42:27 -0600

    statements

---
+++ 14_ifstatments.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    if(3 < 4){
+        echo "3 is less than 10";
+    } else {
+        echo "3 is not less than 10";
+    }
+    ?>
+</body>
+</html>
