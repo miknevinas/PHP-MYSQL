7589e8376ef6333f0f11889b994cb63d67be03a0
Adam <miknevinas@gmail.com>
2016-03-30 11:35:20 -0600

    practice 4

--- _practical-app/4.php
+++ _practical-app/4.php
@@ -15,14 +15,23 @@
 
 	
 	<?php  
+    
+   /* Step1: Define a function and make it return a calculation of 2 numbers
 
-/*  Step1: Define a function and make it return a calculation of 2 numbers
+    Step 2: Make a function that passes parameters and call it using parameter values*/
+    
+    //Step 1
+    function add() {
+        return 1 + 2;
+    }
+    add();
 
-	Step 2: Make a function that passes parameters and call it using parameter values
-
-
- */
-
+    //Step 2
+    function calculate($num1, $num2) {
+        return $num1 * $num2;
+    }
+    $result = calculate(3, 4);
+    echo $result
 	
 ?>
 
