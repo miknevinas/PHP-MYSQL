a2b0bea014f592ac0aaeebe8c6eef249a09cc8ba
Adam <miknevinas@gmail.com>
2016-03-30 11:49:52 -0600

    math functions

---
+++ 27_built_in_math.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    echo pow(2,4);
+    echo "<br />";
+    
+    echo rand(1, 1000);
+    echo "<br />";
+    
+    echo sqrt(100);
+    echo "<br />";
+    
+    echo ceil(2.3);
+    echo "<br />";
+    
+    echo floor(7.7);
+    echo "<br />";
+    
+    ?>
+</body>
+</html>
