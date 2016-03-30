41e7e2ef90c9ee835ddb07c24bb0f16d5396cd91
Adam <miknevinas@gmail.com>
2016-03-30 10:07:48 -0600

    while

---
+++ 17_while.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    $counter = 0;
+    while($counter < 10) {
+        echo "not yet";
+        $counter += 1;
+    }
+    ?>
+</body>
+</html>
