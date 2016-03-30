83072f0b5e0a9bc2c170ea2b9c3e0bdfef083f60
Adam <miknevinas@gmail.com>
2016-03-30 12:02:41 -0600

    array functions

---
+++ 29_array_functions.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $list = [1,2,3,4,5];
+    
+    echo max($list);
+    echo "<br />";
+    
+    echo min($list);
+    echo "<br />";
+    
+    echo sort($list);
+    echo "<br />";
+    
+    ?>
+</body>
+</html>
