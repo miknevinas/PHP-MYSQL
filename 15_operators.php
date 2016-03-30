7b90da913bd6eedfad97d024d6560a93562f52bc
Adam <miknevinas@gmail.com>
2016-03-30 09:51:30 -0600

    comparison

---
+++ 15_operators.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    if(4 > 4 || 5 > 4) {
+        echo "it is true";
+    }
+    
+    ?>
+</body>
+</html>
