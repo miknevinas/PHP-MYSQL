4a459e85710d663d4e4e8854b1e8c7ca85c6b728
Adam <miknevinas@gmail.com>
2016-03-30 11:55:04 -0600

    string functions

---
+++ 28_string_functions.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $string = "Hello";
+    
+    //Finds string length
+    echo strlen($string);
+    
+    echo "<br />";
+    
+    echo strtoupper($string);
+    ?>
+</body>
+</html>
