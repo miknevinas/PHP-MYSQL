4de1e91f9cf60abb6c3335bb752fa93bebacdcba
Adam <miknevinas@gmail.com>
2016-03-31 15:43:51 -0600

    practice 7

--- _practical-app/7.php
+++ _practical-app/7.php
@@ -14,25 +14,45 @@
 
 	<article class="main-content col-xs-8">
 	
-	
+<!--
+        Step 1 - Create a database in PHPmyadmin
+
+        Step 2 - Create a table like the one from the lecture
+
+        Step 3 - Insert some Data
+
+        Step 4 - Connect to Database and read data 
+-->
 	
 	<?php  
+	//Step 4
+        $connection = mysqli_connect('localhost', 'root', '', 'practice7');
 
-	/*  Step 1 - Create a database in PHPmyadmin
+        if($connection) {
+            echo "Connection established";
+        } else {
+            die("Connection failure");
+        }
+        
+        $query = "SELECT * FROM people";
 
-		Step 2 - Create a table like the one from the lecture
-
-		Step 3 - Insert some Data
-
-		Step 4 - Connect to Database and read data
-
-*/
+        $result = mysqli_query($connection, $query);
+        if(!$result) {
+            die('Query failed' . mysqli_error());
+        }
 	
 	?>
-
-
-
-
+        <div class="col-xs-6">
+            <?php 
+            while($row = mysqli_fetch_assoc($result)) {
+            ?>
+            <pre>
+                <?php print_r($row);?>
+            </pre>
+            <?php
+            }
+            ?>
+        </div>
 
 </article><!--MAIN CONTENT-->
 
