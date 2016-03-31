870017956bb90f62f027221a0f5f848af6387c25
Adam <miknevinas@gmail.com>
2016-03-31 13:10:05 -0600

    continue building update query

---
+++ _36_mysql/47b_functions.php
@@ -0,0 +1,19 @@
+<?php include "45b_db.php";?>
+<?php
+function showAllData() {
+    global $connection;
+    //Get users
+    $query = "SELECT * FROM users";
+    //Send information to database
+    $result = mysqli_query($connection, $query);
+    if(!$result) {
+        die('Query failed' . mysqli_error());
+    }
+
+    //Get id's from database
+    while($row = mysqli_fetch_assoc($result)) {
+        $id = $row['id'];
+        echo "<option value='id'>$id</option>";
+    }
+}
+?>
