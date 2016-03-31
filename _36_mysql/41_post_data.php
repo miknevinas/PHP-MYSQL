292b5053427cff60771007568603f16c5a364e92
Adam <miknevinas@gmail.com>
2016-03-31 11:13:40 -0600

    post data

---
+++ _36_mysql/41_post_data.php
@@ -0,0 +1,43 @@
+<?php 
+if(isset($_POST['submit'])) {
+    
+   $username = $_POST['username'];
+   $password = $_POST['password'];
+    
+   echo $username . "<br />" . $password;
+}
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <title>Document</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="col-xs-6">
+                <form action="41_post_data.php" method="post">
+                    <div class="form-group">
+                        <label for="username">Username</label>
+                        <input type="text" name="username" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" name="password" class="form-control">
+                    </div>
+
+                    <input class="btn btn-primary" type="submit" name="submit" value="Submit">
+                </form>
+            </div>
+
+        </div>
+
+    </body>
+</html>
