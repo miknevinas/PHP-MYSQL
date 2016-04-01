13dcced8007542ebc7e113f07e6952376e6f55c6
Adam <miknevinas@gmail.com>
2016-04-01 08:58:10 -0600

    post

---
+++ 59_post.php
@@ -0,0 +1,19 @@
+<?php 
+echo $_POST['name'];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <form action="59_post.php" method="post">
+        
+        <input type="text" name="name">
+        <input type="submit">
+    </form>
+</body>
+</html>
