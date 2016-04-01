2b42a8a7d9749135f2396781eef423da397f6798
Adam <miknevinas@gmail.com>
2016-04-01 08:40:08 -0600

    practice 8

--- _practical-app/8.php
+++ _practical-app/8.php
@@ -14,18 +14,25 @@
 		
 	<article class="main-content col-xs-8">
 	
+        <!--Step 1 -Make a variable with some text as value
+
+        Step 2 - Use crypt() function to encrypt it
+
+        Step 3 - Assign the crypt result to a variable
+
+        Step 4 - echo the variable-->
 	
 	<?php  
-
-	/*  Step 1 -Make a variable with some text as value
-
-		Step 2 - Use crypt() function to encrypt it
-
-		Step 3 - Assign the crypt result to a variable
-
-		Step 4 - echo the variable
-
-	*/
+        //Step 1
+        $password = "123";
+        //Step 2
+        $hashFormat = '$2y$10$';
+        $salt = 'mdksndje839202ksjduenr';
+        $hashAndSalt = $hashFormat . $salt;
+        //Step 3
+        $password = crypt($password, $hashAndSalt);
+        //Step 4
+        echo $password;
 	
 	?>
 
