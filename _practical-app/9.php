51ef76a5ba4e32032b521463a6e790d943078ae4
Adam <miknevinas@gmail.com>
2016-04-01 10:02:38 -0600

    practice 9

--- _practical-app/9.php
+++ _practical-app/9.php
@@ -15,20 +15,32 @@
 
 			<article class="main-content col-xs-8">
 			
+        <!--Create a link saying Click Here, and set 
+        the link href to pass some parameters and use the GET super global to see it
+
+        Step 2 - Set a cookie that expires in one week
+
+        Step 3 - Start a session and set it to value, any value you want.-->
 		
 	
 	<?php 
-
-	/*  Create a link saying Click Here, and set 
-	the link href to pass some parameters and use the GET super global to see it
-
-		Step 2 - Set a cookie that expires in one week
-
-		Step 3 - Start a session and set it to value, any value you want.
-	*/
-	
-	?>
-
+        //Step 1
+	    if(isset($_GET['source'])) {
+            echo $_GET['source']; 
+        }
+        
+        //Step 2
+        $name = "thename";
+        $value = 100;
+        $expiration = time() + (60 * 60 * 24 * 7);
+        setcookie($name, $value, $expiration);
+        
+        //Step 3
+        session_start();
+        $_SESSION['greeting'] = "Hello student";
+    ?>
+    
+    <a href ="9.php?source=3000">CLICK HERE</a>
 
 
 
