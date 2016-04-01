48b9f527cc827b27296e64f9c5937b755bb0941f
Adam <miknevinas@gmail.com>
2016-04-01 09:12:06 -0600

    setting cookies

---
+++ 61_cookies.php
@@ -0,0 +1,18 @@
+<?php 
+$name = "thename";
+$value = 100;
+$expiration = time() + (60 * 60 * 24 * 7);
+setcookie($name, $value, $expiration);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
